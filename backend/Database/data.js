69ad5b62fec35ff7e22bfbb4ad6a6a9bf86f802c
Aman5Techie <vermaji9585@gmail.com>
2024-05-25 15:41:20 +0530

    database

--- backend/Database/data.js
+++ backend/Database/data.js
@@ -1,18 +1,16 @@
-
 const temp = {
-    title : "Temp Task 1",
+    title : "Post on Instagram",
     due : "03-Oct",
     status : "pending",
     id : 1526,
 }
 const temp2 = {
-    title : "Temp Task 2",
+    title : "Do remaining Course",
     due : "04-Oct",
     status : "pending",
     id : 2025,
 }
 
 const todos = [temp,temp2]
-
 
 module.exports = todos
